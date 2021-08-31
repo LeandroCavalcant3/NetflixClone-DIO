cbf64611d8ebc378bd97b263712ec12f86796157
Leandro Cavalcante <88688238+LeandroCavalcant3@users.noreply.github.com>
2021-08-31 17:25:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function clicou(){
+    open("https://www.youtube.com/watch?v=ESEUoa-mz2c");
+}
+
+function clicouu(){
+    open("https://www.netflix.com/br/title/80100172")
+}
